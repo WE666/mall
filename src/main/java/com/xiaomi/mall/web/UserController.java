2a9b0c39b4f9da037c6824e6b68da04f9bb5c4b2
lcy <1728652210@qq.com>
2018-11-15 17:13:22 +0800

    更新

--- src/main/java/com/xiaomi/mall/web/UserController.java
+++ src/main/java/com/xiaomi/mall/web/UserController.java
@@ -6,7 +6,6 @@ import com.xiaomi.mall.entity.UserEntity;
 import com.xiaomi.mall.repository.UserRepository;
 import com.xiaomi.mall.utils.DateUtils;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -23,19 +22,20 @@ public class UserController {
     @Autowired
     private UserRepository userRepository;
 
-    @RequestMapping(value = "/register",method = RequestMethod.POST)
-    public Response register(UserEntity userEntity){
+
+    @RequestMapping(value = "/doregist",method = RequestMethod.POST)
+    public String doregist(UserEntity userEntity){
         try {
             UserEntity registUser = userRepository.findByUsername(userEntity.getUsername());
-            if (registUser == null){
-                return new Response(ExceptionMsg.UserNameUsed);
+            if (registUser != null){
+                return "该昵称已被占用";
             }
             userEntity.setCreated(DateUtils.getCurrentTime());
             userEntity.setUpdated(DateUtils.getCurrentTime());
             userRepository.save(userEntity);
         }catch (Exception e){
-            return new Response(ExceptionMsg.FAILED);
+            return "注册失败";
         }
-        return new Response();
+        return "注册成功";
     }
 }
